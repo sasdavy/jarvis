b243bd32f78790631e0054e35348272c73949ab5
sasdavy <sas.davy@outlook.com>
2016-03-17 13:43:09 +0800

    add the personal main page

--- resources/js/loginCheck.js
+++ resources/js/loginCheck.js
@@ -1,4 +1,9 @@
 function checkLogin()
 {
     var userName = document.forms["loginForm"]["email"].value;
+    if (userName.indexof("@")<1)
+    {
+        window.alert("User Name Input Error!");
+        return false;
+    }
 }
